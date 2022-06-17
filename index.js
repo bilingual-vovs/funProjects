b3e3ebfcb883863b2741f47653a3ba725e86ddb8
bilingual-vovs <vovavovsart@gmail.com>
2022-06-17 19:26:38 +0300

    CPS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let cps = document.getElementById("result")
 let cpsBox = document.getElementById("cps-div")
 let cpCount = 0
+let time = 0
 function cpIncrement(event){
     cpCount++
     cps.innerHTML = cpCount
@@ -16,6 +17,34 @@ function clearCSP(){
 
     cpCount = 0
 }
+let graph = {
+    data: [],
+    get sizeX(){
+        return document.getElementById("graph-div").clientWidth
+    },
+    get sizeY(){
+        return document.getElementById("graph-div").clientHeight
+    },
+    get height(){
+        let height = 7
+        this.data.forEach(elem =>{
+            if (height < elem) height = elem
+        })
+        return height
+    },
+    update(newPix){
+        let width = this.sizeX
+        let height = this.sizeY
+        let graphHeight = this.height
+        let container = document.getElementById("graph-div")
+        container.innerHTML = ''
+        this.data.push(newPix)
+        this.data.forEach(elem => {
+            container.innerHTML += `<div style='position:absolute;top:${height/graphHeight*elem};left:${width/10*(width/10 - elem)};'></div>`
+        })
+    }
+}
+
 cpsBox.addEventListener("click",(event)=>{
     console.log("test")
     cpIncrement(event)
